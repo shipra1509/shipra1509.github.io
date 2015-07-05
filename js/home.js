f6fff91a85087b769ce70566f0ada216f8fbfdf9
shipra <shipra.juet@gmail.com>
2015-07-06 00:18:23 +0530

    initial commit

---
+++ js/home.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+ $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.href);
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+new WOW().init();
